0f8ce150fdc54f8f1401cd8b0099ee56929bd72f
Enrique Antezana Vargas <enrique.antezana@jalasoft.com>
2023-10-29 17:20:31 -0400

    Define required modules and services for the testsuite using createTestingModule

--- src/users/users.resolver.spec.ts
+++ src/users/users.resolver.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { CatsService } from 'src/cats/cats.service';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService],
+      providers: [UsersResolver, UsersService, CatsService],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,13 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CatsModule } from 'src/cats/cats.module';
+import { UsersModule } from 'src/users/users.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [UsersModule, CatsModule, AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
